c9822d72e35d0dd513d69590a99bb41d3da3aad5
Akshat <akshatjangir1710@gmail.com>
2025-01-29 11:29:21 +0530

    done

--- src/components/mdx/mdx-component.tsx
+++ src/components/mdx/mdx-component.tsx
@@ -1,52 +1,109 @@
-'use client'
-import { getMDXComponent } from 'mdx-bundler/client'
-import Image from 'next/image'
-import { useMemo } from 'react'
-import { CustomCode, Pre } from './custom-code'
-import CustomLink from './custom-link'
+// "use client";
+// import { getMDXComponent } from "mdx-bundler/client";
+// import Image from "next/image";
+// import { useMemo } from "react";
+// import { CustomCode, Pre } from "./custom-code";
+// import CustomLink from "./custom-link";
+
+// const MDXComponentsMap = {
+//   a: CustomLink,
+//   Image,
+//   img: ({ ...props }: any) => {
+//     console.log("props", props);
+//     return <img className="border rounded-lg" {...props} />;
+//   },
+//   // video: ({ src, controls = true, ...props }: any) => (
+//   //   <video
+//   //     className="rounded-lg border mt-4"
+//   //     src={src}
+//   //     controls={controls}
+//   //     {...props}
+//   //   >
+//   //     Your browser does not support the video tag.
+//   //   </video>
+//   // ),
+
+//   pre: Pre,
+//   code: CustomCode,
+// };
+
+// console.log("MDX component is", MDXComponentsMap);
+
+// type MDXComponentProps = {
+//   content: string;
+//   components?: Record<string, any>;
+// };
+
+// export const MDXComponent = ({
+//   content,
+//   components = {},
+// }: MDXComponentProps) => {
+//   console.log("content", content);
+//   const Component = useMemo(() => getMDXComponent(content), [content]);
+
+//   return (
+//     <Component
+//       components={
+//         {
+//           ...MDXComponentsMap,
+//           ...components,
+//         } as any
+//       }
+//     />
+//   );
+// };
+
+// export default MDXComponent;
+
+"use client";
+import { getMDXComponent } from "mdx-bundler/client";
+import Image from "next/image";
+import { useMemo } from "react";
+import { CustomCode, Pre } from "./custom-code";
+import CustomLink from "./custom-link";
 
 const MDXComponentsMap = {
   a: CustomLink,
   Image,
-  img: ({ ...props }: any) => <img className="border rounded-lg" {...props} />,
-  // video: ({ src, controls = true, ...props }: any) => (
-  //   <video
-  //     className="rounded-lg border mt-4"
-  //     src={src}
-  //     controls={controls}
-  //     {...props}
-  //   >
-  //     Your browser does not support the video tag.
-  //   </video>
-  // ),
-
+  img: ({ src, alt, ...props }: any) => {
+    // Check if the image is actually a video placeholder
+    console.log("src alt", src, alt);
+    return (
+      <video
+        className="rounded-lg border mt-4 w-full"
+        src={alt} // Use the alt text as video URL
+        controls
+        {...props}
+      >
+        Your browser does not support the video tag.
+      </video>
+    );
+  },
   pre: Pre,
-  code: CustomCode
-}
-
-console.log("MDX component is", MDXComponentsMap)
+  code: CustomCode,
+};
 
 type MDXComponentProps = {
-  content: string
-  components?: Record<string, any>
-}
+  content: string;
+  components?: Record<string, any>;
+};
 
 export const MDXComponent = ({
   content,
-  components = {}
+  components = {},
 }: MDXComponentProps) => {
-  const Component = useMemo(() => getMDXComponent(content), [content])
+  const Component = useMemo(() => getMDXComponent(content), [content]);
 
   return (
     <Component
       components={
         {
           ...MDXComponentsMap,
-          ...components
+          ...components,
         } as any
       }
     />
-  )
-}
+  );
+};
 
-export default MDXComponent
+export default MDXComponent;

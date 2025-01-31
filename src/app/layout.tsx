1bd3e2558b45bf8587f9093cae5fe5787f3648b4
Akshat171 <akshatjangir1710@gmail.com>
2025-01-31 11:41:19 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <BuiltWithOutstatic />
         </ThemeProvider>
       </body>
     </html>
